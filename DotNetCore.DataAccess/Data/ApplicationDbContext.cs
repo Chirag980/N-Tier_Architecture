96fd990be662c35a355be62f55cbfeac10c62fdc
Chirag980 <chiragsahu980@gmail.com>
2024-09-02 08:32:03 +0530

    Crate a DAL layed Data Access Layer in which we have Data folder and one migration folder which comes through Entity FrameWork

---
+++ DotNetCore.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DotNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCore.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+        //How to create a table by using Entity framework?:
+            //From here we are creating table that's the beauty of the entity framework by migration
+            // here is the first step to create a table summarizing the model:
+            // First we need to create a model for now it's Category.cs file and then we have to set the validations and the primary key identity property on that model.
+            // After the model creation we have to set the DBSet for that public DbSet<Category> Categories { get; set; }
+            // After this we have to open the tools-->Nuget Package Manager--> Click on the Package Manager Console--> Write a command that Add-Migration
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1,Name="Action",DisplayOrder=1},
+            new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "History", DisplayOrder = 3 } );
+        }
+    }
+}
---
+++ DotNetCore.DataAccess/Migrations/20240519072844_rename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCore.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class rename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CategoryType",
+                table: "Categories",
+                newName: "DisplayOrder");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DisplayOrder",
+                table: "Categories",
+                newName: "CategoryType");
+        }
+    }
+}
